9f787bd9bd9e083b5876c734a4b65790ff59c02f
Ayush Kumar <68886542+Hi-Ayush@users.noreply.github.com>
2020-12-22 13:26:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,22 @@ const port = 8080
 
 const onePageArticleCount = 10
 
-
+const {newsArticleModel}=require("./connector");
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.get("/newsFeed",(req,res)=>{
+    let limit=parseInt(req.body.limit);
+    let offset=parseInt(req.body.offset);
 
+    if(isNaN(limit)||isNaN(offset)){
+        limit=onePageArticleCount;
+        offset=0;
+    }
+
+    newsArticleModel.find().limit(limit).skip(offset).then(result=>res.send(result));
+})
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
